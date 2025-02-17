5e1ebdd37c8f32adcebd0bedf678f0b1402c9c5b
Shubham-Chakraborty <chakrabortyshubham00@gmail.com>
2025-02-17 10:22:10 +0000

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let string = str.toLowerCase();
+    let left = 0, right = string.length-1;
+    while (left <= right) {
+        if (string.at(left) == " " || string.at(left) == "." ||string.at(left) == "," ||string.at(left) == "!") {
+            ++left;
+            continue;
+        }
+        if (string.at(right) == " " || string.at(right) == "." ||string.at(right) == "," ||string.at(right) == "!") {
+            --right;
+            continue;
+        }
+        if (string.at(left) != string.at(right)) {
+            return false;
+        }
+        ++left;
+        --right;
+    }
+    return true;
 };
 
 // Do not edit below this line
