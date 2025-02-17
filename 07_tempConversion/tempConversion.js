f43ed6753922088a1d56559fa0e52f45b2a7c509
Shubham-Chakraborty <chakrabortyshubham00@gmail.com>
2025-02-17 09:45:19 +0000

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let cel = (fahr - 32) * 5/9;
+  cel = Math.round(cel * 10)/10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahr = (cel * 9/5) + 32;
+  fahr = Math.round(fahr * 10)/10;
+  return fahr;
 };
 
 // Do not edit below this line
