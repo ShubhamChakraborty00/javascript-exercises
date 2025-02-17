1b341a6c97417652498d7364dae35f18a09787e3
Shubham-Chakraborty <chakrabortyshubham00@gmail.com>
2025-02-17 09:15:10 +0000

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newarr = [];
+    for (const element of arr)  {
+        if (args.includes(element)) {
+            continue;
+        }
+        newarr.push(element);
+    }
+    return newarr;
 };
 
 // Do not edit below this line
