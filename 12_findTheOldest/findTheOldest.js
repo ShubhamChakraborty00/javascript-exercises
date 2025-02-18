a2f14f769d310ef4c7804277dbe369a52eac5ec1
Shubham-Chakraborty <chakrabortyshubham00@gmail.com>
2025-02-18 06:15:04 +0000

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let nameOfOldest;
+    let maxAge = 0;
+    let d = new Date();
+    d = d.getFullYear();
+    for (let i=0; i<arr.length; ++i) {
+        if (arr[i]['yearOfDeath'] == undefined) {
+            arr[i]['yearOfDeath'] = d;
+        }
+        if ((arr[i]['yearOfDeath'] - arr[i]['yearOfBirth']) > maxAge) {
+            maxAge = arr[i]['yearOfDeath'] - arr[i]['yearOfBirth'];
+            nameOfOldest = arr[i];
+        }
+    }
 
+    return nameOfOldest;
 };
 
 // Do not edit below this line
